82632cff5b4fb5e73de705729be4e39c6583307f
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-10-25 19:02:17 -0300

    Adds input component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,22 @@
 
-import { Title } from "_/components";
+import { useState } from "react";
+import { Input, Title } from "_/components";
 
 export function Home(){
+    const [value, setValue] = useState('')
+
     return(
         <>
             <Title>
                 Home Page
             </Title>
+
+            <Input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="placeholder"
+                label="write here:"
+            />
         </>
     )
 }
