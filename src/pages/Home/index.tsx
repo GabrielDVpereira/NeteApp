3a46bca512c5d19a220c502ccd08ff93fcbb4cf9
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-10-25 19:05:31 -0300

    Revert "Adds input component"

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,12 @@
 
-import { useState } from "react";
-import { Input, Title } from "_/components";
+import { Title } from "_/components";
 
 export function Home(){
-    const [value, setValue] = useState('')
-
     return(
         <>
             <Title>
                 Home Page
             </Title>
-
-            <Input
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-                placeholder="placeholder"
-                label="write here:"
-            />
         </>
     )
 }
