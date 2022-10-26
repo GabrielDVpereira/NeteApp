1a0f312ebd12a47fea00239fea80c585365280b3
GabrielDvPereira <gdavi.gd@gmail.com>
2022-10-26 20:37:03 -0300

    feat: create booking service

---
+++ src/services/bookingService.ts
@@ -0,0 +1,34 @@
+import { AlertHelper } from "_/helpers"
+import { Booking } from "_/models/booking"
+import { DatabaseRepository } from "_/repositories"
+
+export interface IBookingService {
+    createBooking(booking: Booking): Promise<void>
+    listBookings(): Promise<Booking[] | undefined>
+}
+
+export class BookingService implements IBookingService {
+    constructor(
+        private readonly bookingDatabaseRepository: DatabaseRepository, 
+        private readonly alertHelper: AlertHelper
+    ){}
+
+    async createBooking(booking: Booking): Promise<void> {
+        try {
+            await this.bookingDatabaseRepository.create(booking);
+        } catch(err){
+            console.error(err)
+            this.alertHelper.alertError("Não foi possível agendar sua reserva.")
+        }
+    }
+    async listBookings(): Promise<Booking[] | undefined> {
+        try {
+            const bookings = await this.bookingDatabaseRepository.getAll<Booking>()
+            return bookings
+        } catch(err){
+            console.error(err)
+            this.alertHelper.alertError("Não foi possível recuperar as reservas.")
+        }
+    }
+    
+}
