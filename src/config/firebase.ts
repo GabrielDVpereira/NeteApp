bada947c26310ffd956c134cde11f9f13c52c773
GabrielDvPereira <gdavi.gd@gmail.com>
2022-10-25 22:22:08 -0300

    chore: add configuration for firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIRABSE_API_KEY,
+    authDomain: process.env.REACT_APP_FIRABSE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIRABSE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIRABSE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIRABSE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIRABSE_MESSAGING_APP_ID,
+    measurementId: process.env.REACT_APP_FIRABSE_MEASUREMENT_ID
+  };
+
+  const app = initializeApp(firebaseConfig);
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import '_/config/firebase'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
