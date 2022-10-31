4787b6b0648ec6b1f464e6b3710e50e34a4fcbd5
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-10-31 20:01:16 -0300

    Adds checkin context provider to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { ToastContainer } from "react-toastify";
 import { AppRoutes } from "_/routes";
-import { AuthContextProvider, BookingContextProvider } from "./contexts";
+import { AuthContextProvider, BookingContextProvider, CheckinContextProvider } from "./contexts";
 import { AlertHelper } from "./helpers/alert";
 import { FirebaseDatabaseRepository } from "./repositories";
-import { AuthService, BookingService } from "./services";
+import { AuthService, BookingService, CheckinService } from "./services";
 
 function App() {
 
   const alertHelper = new AlertHelper()
   const authService = new AuthService(alertHelper)
 
-  const bookinRepository = new FirebaseDatabaseRepository('booking')
-  const bookingService = new BookingService(bookinRepository, alertHelper)
+  const bookingRepository = new FirebaseDatabaseRepository('booking')
+  const bookingService = new BookingService(bookingRepository, alertHelper)
+
+  const checkinRepository = new FirebaseDatabaseRepository('checkin')
+  const checkinService = new CheckinService(checkinRepository, alertHelper)
 
   return (
     <AuthContextProvider authService={authService}>
       <BookingContextProvider bookingService={bookingService}>
-        <AppRoutes />
-        <ToastContainer theme="colored" />
+        <CheckinContextProvider  checkinService={checkinService}>
+          <AppRoutes />
+          <ToastContainer theme="colored" />
+        </CheckinContextProvider>
       </BookingContextProvider>
     </AuthContextProvider>
   );
