392b9b32b7a50090c6dcaa77be1064b3f77df918
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-11-27 14:27:52 -0300

    Add database watching method for checkin and bookings
    Signed-off-by: Gabriela Guedes <gabrielabguedes@gmail.com>

--- src/repositories/database.ts
+++ src/repositories/database.ts
@@ -1,16 +1,31 @@
-import { getFirestore, setDoc, doc, Firestore, collection, query, getDocs, QueryConstraint, where, Query, DocumentData  } from 'firebase/firestore';
+import { getFirestore, setDoc, doc, Firestore, collection, query, getDocs, where, Query, DocumentData, onSnapshot, QuerySnapshot, Unsubscribe, DocumentSnapshot  } from 'firebase/firestore';
 
 
 export interface DatabaseRepository {
     getAll<T>(): Promise<T[]>
     create(data: any): Promise<void>
     findBy<T>(field: string, value: string): Promise<T[]>
+    watch(callback: (data: any) => void): void
+    unsubscribe(): void
 }
 
 export class FirebaseDatabaseRepository implements DatabaseRepository {
     private readonly firestore: Firestore = getFirestore()
+    private unsub: Unsubscribe | null = null
 
     constructor( private readonly collection: string){}
+
+    async watch(callback: (data: any) => void){
+        this.unsub = onSnapshot(collection(this.firestore, this.collection), async (snap) => {
+            const data: any[] = snap.docs.map(doc => doc.data())
+            callback(data)
+        })
+    }
+
+    async unsubscribe(){
+        if(this.unsub === null) return
+        this.unsub()
+    }
 
     async getAll<T>(): Promise<T[]> {
         const docsRef = query(collection(this.firestore, this.collection));
@@ -28,9 +43,8 @@ export class FirebaseDatabaseRepository implements DatabaseRepository {
         return await this.parseQueryResult(docsRef)
     }
 
-    async parseQueryResult<T>(query: Query<DocumentData>){
+    async parseQueryResult<T>(query: Query<DocumentData>):Promise<T[]>{
         const docsSnap = await getDocs(query)
-
         const result: T[] = []
 
         docsSnap.forEach(snap => {
