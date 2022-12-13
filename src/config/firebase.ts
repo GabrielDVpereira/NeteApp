d43979cea3fdca2c3c6e9a84b69de1ba4cd98c94
Gabriela Guedes <gabrielabguedes@gmail.com>
2022-12-13 19:15:51 -0300

    Rename the environment variables due to typo

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIRABSE_API_KEY,
-    authDomain: process.env.REACT_APP_FIRABSE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIRABSE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIRABSE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIRABSE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIRABSE_MESSAGING_APP_ID,
-    measurementId: process.env.REACT_APP_FIRABSE_MEASUREMENT_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 
 export const app = initializeApp(firebaseConfig);
